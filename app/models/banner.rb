29bc191cd48f3fe342015b01863831c2f3f4a278
QW3 <contato@qw3.com.br>
2015-01-27 17:31:31 -0200

    Adicionado validação

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -2,6 +2,11 @@ class Banner < ActiveRecord::Base
   
   has_attached_file :imagem, :styles => { :small => "100x100", :thumb => "40x40" }, :path => "public/images/banners/banner_#{:banner_id}/:id/:style/:filename", :url => "/images/banners/banner_#{:banner_id}/:id/:style/:filename", :default_url => "#{Settings["PAPERCLIP.missing_path"]}/:style.png"
   
+  validates_attachment :imagem,
+    presence: true,
+    size: { in: 0..2.megabytes },
+    content_type: { content_type: ["image/jpg", "image/jpeg", "image/gif", "image/png",  "image/tiff"] }
+  
   default_scope :order => "ordem"
   
   before_create :atribuir_ordem
